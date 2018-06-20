7b041f27115789e6aeafefd32e9a725b1804239c
sofia ongele <sofiaongele@gmail.com>
2018-06-20 16:43:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+
+function getUKTime() {
+  hours = new Date().getHours() + 7
+  return hours
+}
+
+function discountTicket(ogPrice) {
+  newPrice = Number.parseInt(ogPrice) * 0.8
+  return newPrice
+}
+
+function tweetReply(handle) {
+  return `Hey ${handle}, thanks so much for the support! I really appreciate it!`
+}
